dd0d7a4c79536e2a162374da35e2194cd128e353
anhletel4vn <tuananh@tel4vn.com>
2024-01-17 22:52:14 +0700

    update - fix conversation

--- model/conversation.go
+++ model/conversation.go
@@ -5,22 +5,23 @@ import (
 )
 
 type Conversation struct {
-	ConversationId   string          `json:"conversation_id"`
-	ConversationType string          `json:"conversation_type"`
-	AppId            string          `json:"app_id"`
-	OaId             string          `json:"oa_id"`
-	UserIdByApp      string          `json:"user_id_by_app"`
-	Uid              string          `json:"uid"`
-	Username         string          `json:"username"`
-	PhoneNumber      string          `json:"phone_number"`
-	Email            string          `json:"email"`
-	Avatar           string          `json:"avatar"`
-	IsDone           bool            `json:"is_done"`
-	IsDoneAt         int64           `json:"is_done_at"`
-	IsDoneBy         json.RawMessage `json:"is_done_by"`
-	CreatedAt        string          `json:"created_at"`
-	UpdatedAt        string          `json:"updated_at"`
-	TotalUnRead      int64           `json:"total_unread"`
+	ConversationId       string          `json:"conversation_id"`
+	ConversationType     string          `json:"conversation_type"`
+	AppId                string          `json:"app_id"`
+	OaId                 string          `json:"oa_id"`
+	UserIdByApp          string          `json:"user_id_by_app"`
+	Uid                  string          `json:"uid"`
+	Username             string          `json:"username"`
+	PhoneNumber          string          `json:"phone_number"`
+	Email                string          `json:"email"`
+	Avatar               string          `json:"avatar"`
+	IsDone               bool            `json:"is_done"`
+	IsDoneAt             int64           `json:"is_done_at"`
+	IsDoneBy             json.RawMessage `json:"is_done_by"`
+	CreatedAt            string          `json:"created_at"`
+	UpdatedAt            string          `json:"updated_at"`
+	TotalUnRead          int64           `json:"total_unread"`
+	LatestMessageContent string          `json:"latest_message_content"`
 }
 
 type ElasticsearchChatResponse struct {
