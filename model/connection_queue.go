7ce7e80aa6c7eff7379807f7a088a9567e66f3c1
anhletel4vn <tuananh@tel4vn.com>
2024-01-26 11:39:01 +0700

    update - connection queue

--- model/connection_queue.go
+++ model/connection_queue.go
@@ -4,8 +4,10 @@ import "github.com/uptrace/bun"
 
 type ConnectionQueue struct {
 	*Base
-	bun.BaseModel `bun:"table:connection_queue,alias:cq"`
-	ConnectionId  string `json:"connection_id" bun:"connection_id,type:text,notnull"`
-	QueueId       string `json:"queue_id" bun:"queue_id,type:text,notnull"`
-	Status        string `json:"status" bun:"status,notnull"`
+	bun.BaseModel     `bun:"table:connection_queue,alias:cq"`
+	ConnectionId      string             `json:"connection_id" bun:"connection_id,type:text,notnull"`
+	ChatConnectionApp *ChatConnectionApp `json:"chat_connection_app" bun:"rel:has-one,join:connection_id=id"`
+	QueueId           string             `json:"queue_id" bun:"queue_id,type:text,notnull"`
+	ChatQueue         ChatQueue          `json:"chat_queue" bun:"rel:has-one,join:queue_id=id"`
+	Status            string             `json:"status" bun:"status,notnull"`
 }
