425d3b4c897935531cec3f66a55d63135dc1a4bb
anhletel4vn <tuananh@tel4vn.com>
2024-02-19 16:31:02 +0700

    update - fix table uuid

--- model/chat_queue.go
+++ model/chat_queue.go
@@ -11,7 +11,7 @@ type ChatQueue struct {
 	bun.BaseModel    `bun:"table:chat_queue,alias:cq"`
 	QueueName        string             `json:"queue_name" bun:"queue_name,type:text,notnull"`
 	Description      string             `json:"description" bun:"description,type:text"`
-	ChatRoutingId    string             `json:"chat_routing_id" bun:"chat_routing_id,type:text,notnull"`
+	ChatRoutingId    string             `json:"chat_routing_id" bun:"chat_routing_id,type:uuid,notnull"`
 	ChatRouting      *ChatRouting       `json:"chat_routing" bun:"rel:has-one,join:chat_routing_id=id"`
 	ConnectionQueues []*ConnectionQueue `json:"connection_queues" bun:"rel:has-many,join:id=queue_id"`
 	ChatQueueAgent   []*ChatQueueAgent  `json:"chat_queue_agent" bun:"rel:has-many,join:id=queue_id"`
