738e9fc325fe72c181ec9def452d2ef66caf163e
anhletel4vn <tuananh@tel4vn.com>
2024-02-28 23:59:51 +0700

    update - enable s3

--- repository/repository.go
+++ repository/repository.go
@@ -73,7 +73,10 @@ func InitTables(ctx context.Context, dbConn sqlclient.ISqlClientConn) {
 }
 
 func InitColumn(ctx context.Context, db sqlclient.ISqlClientConn) {
-	if _, err := db.GetDB().NewAddColumn().Model((*model.ChatApp)(nil)).IfNotExists().ColumnExpr("set_default_app TEXT NOT NULL").Exec(ctx); err != nil {
+	if _, err := db.GetDB().NewAddColumn().Model((*model.ChatApp)(nil)).IfNotExists().ColumnExpr("set_default_app text").Exec(ctx); err != nil {
+		panic(err)
+	}
+	if _, err := db.GetDB().NewAddColumn().Model((*model.AgentAllocation)(nil)).IfNotExists().ColumnExpr("app_id text").Exec(ctx); err != nil {
 		panic(err)
 	}
 }
