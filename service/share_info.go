69dc7f2b2e627d447653e30032d362e2b0aa5cff
truongnguyentel4vn <truongnguyen@tel4vn.com>
2024-06-13 13:54:39 +0700

    update - move support functions to new file

--- service/share_info.go
+++ service/share_info.go
@@ -5,11 +5,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
-	"mime/multipart"
-	"net/url"
-	"path"
-
 	"github.com/go-resty/resty/v2"
 	"github.com/tel4vn/fins-microservices/common/log"
 	"github.com/tel4vn/fins-microservices/common/response"
@@ -17,6 +12,7 @@ import (
 	"github.com/tel4vn/fins-microservices/internal/storage"
 	"github.com/tel4vn/fins-microservices/model"
 	"github.com/tel4vn/fins-microservices/repository"
+	"mime/multipart"
 )
 
 /**
@@ -284,55 +280,4 @@ func (s *ShareInfo) DeleteShareInfoById(ctx context.Context, authUser *model.Aut
 		return err
 	}
 	return nil
-}
-
-func uploadImageToStorageShareInfo(c context.Context, file *multipart.FileHeader) (url string, err error) {
-	f, err := file.Open()
-	if err != nil {
-		log.Error(err)
-		return
-	}
-	fileBytes, err := io.ReadAll(f)
-	if err != nil {
-		log.Error(err)
-		return
-	}
-	metaData := storage.NewStoreInput(fileBytes, file.Filename)
-	isSuccess, err := storage.Instance.Store(c, *metaData)
-	if err != nil || !isSuccess {
-		log.Error(err)
-		return
-	}
-
-	input := storage.NewRetrieveInput(file.Filename)
-	_, err = storage.Instance.Retrieve(c, *input)
-	if err != nil {
-		log.Error(err)
-		return
-	}
-
-	url = API_DOC + "/bss-message/v1/share-info/image/" + input.Path
-
-	return
-}
-
-func removeFileFromStorageShareInfo(c context.Context, fileUrl string) error {
-	fileName, err := splitFileNameFromUrl(fileUrl)
-	if err != nil {
-		return err
-	}
-
-	input := storage.NewRetrieveInput(fileName)
-	return storage.Instance.RemoveFile(c, *input)
-}
-
-func splitFileNameFromUrl(fileUrl string) (string, error) {
-	// Parse the URL
-	parsedURL, err := url.Parse(fileUrl)
-	if err != nil {
-		fmt.Println("Error parsing URL:", err)
-		return "", err
-	}
-	fileName := path.Base(parsedURL.Path)
-	return fileName, nil
 }
