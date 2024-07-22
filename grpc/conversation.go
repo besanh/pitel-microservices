3767ecaa4ceb67c84da37661da19a24c72e832a9
truongnguyentel4vn <truongnguyen@tel4vn.com>
2024-07-22 11:56:49 +0700

    update - add validation for updating status convo

--- grpc/conversation.go
+++ grpc/conversation.go
@@ -2,12 +2,11 @@ package grpc
 
 import (
 	"context"
-	"errors"
 
-	"github.com/tel4vn/fins-microservices/common/log"
 	"github.com/tel4vn/fins-microservices/common/response"
 	pb "github.com/tel4vn/fins-microservices/gen/proto/conversation"
 	"github.com/tel4vn/fins-microservices/middleware/auth"
+	"github.com/tel4vn/fins-microservices/model"
 	"github.com/tel4vn/fins-microservices/service"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -26,10 +25,13 @@ func (g *GRPCConversation) UpdateStatusConversation(ctx context.Context, request
 	if !ok {
 		return nil, status.Errorf(codes.Unauthenticated, response.ERR_TOKEN_IS_INVALID)
 	}
-
-	if len(request.GetStatus()) < 1 {
-		log.Error("status is required")
-		return nil, status.Errorf(codes.InvalidArgument, errors.New("status is required").Error())
+	payload := model.ConversationStatusRequest{
+		AppId:          request.GetAppId(),
+		ConversationId: request.GetConversationId(),
+		Status:         request.GetStatus(),
+	}
+	if err := payload.Validate(); err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, err.Error())
 	}
 
 	err := service.ConversationService.UpdateStatusConversation(ctx, user, request.GetAppId(), request.GetConversationId(), user.UserId, request.GetStatus())
