5b16fd96bd0fd3e2650c64786ac1792ce86c3d18
truongnguyentel4vn <truongnguyen@tel4vn.com>
2024-08-22 08:54:33 +0700

    update - refactor publish func

--- service/support_message.go
+++ service/support_message.go
@@ -247,3 +247,46 @@ func PublishWsEventToManyUser(eventType, eventDataType string, subscribers []str
 	}
 	wg.Wait()
 }
+
+/*
+ * send event note to high level subscribers
+ */
+func PublishEventToHighLevel(authUser *model.AuthUser, manageQueueUser *model.ChatManageQueueUser, eventName, eventDataType string, data any, levels ...string) {
+	// default send it to admin
+	if len(levels) < 1 {
+		levels = append(levels, variables.ADMIN_LEVEL)
+	}
+
+	subscribers := make(map[string][]string)
+	for sub := range WsSubscribers.Subscribers {
+		if sub.TenantId == authUser.TenantId {
+			if _, ok := subscribers["default"]; !ok {
+				subscribers["default"] = []string{}
+			}
+			subscribers["default"] = append(subscribers["default"], sub.Id)
+			// high level subscriber
+			for _, level := range levels {
+				if _, ok := subscribers[level]; !ok {
+					subscribers[level] = []string{}
+				}
+				if sub.Level == level {
+					subscribers[level] = append(subscribers[level], sub.Id)
+				}
+			}
+		}
+	}
+
+	if manageQueueUser != nil {
+		// Event to manager
+		isExist := BinarySearchSlice(manageQueueUser.UserId, subscribers[variables.MANAGER_LEVEL])
+		if isExist && len(manageQueueUser.UserId) > 0 {
+			go PublishWsEventToOneUser(variables.EVENT_CHAT[eventName], eventDataType, manageQueueUser.UserId, subscribers["default"], true, data)
+		}
+	}
+
+	// Event to admin
+	if ENABLE_PUBLISH_ADMIN && len(subscribers[variables.ADMIN_LEVEL]) > 0 {
+		go PublishWsEventToManyUser(variables.EVENT_CHAT[eventName], eventDataType, subscribers[variables.ADMIN_LEVEL], true, data)
+	}
+	go PublishWsEventToOneUser(variables.EVENT_CHAT[eventName], eventDataType, authUser.UserId, subscribers["default"], true, data)
+}
