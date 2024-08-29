efd5b0c10db9909ebbb734708d8c14a35b7fce6b
truongnguyentel4vn <truongnguyen@tel4vn.com>
2024-08-29 14:32:49 +0700

    update - change table name, limit content to 1000 characters

--- model/note_list.go
+++ model/note_list.go
@@ -8,7 +8,7 @@ import (
 
 type NotesList struct {
 	*Base
-	bun.BaseModel  `bun:"table:notes_list"`
+	bun.BaseModel  `bun:"table:chat_notes_list"`
 	TenantId       string `json:"tenant_id" bun:"tenant_id,type:uuid,notnull"`
 	Content        string `json:"content" bun:"content,type:text,notnull"`
 	ConversationId string `json:"conversation_id" bun:"conversation_id,type:text,notnull"`
@@ -24,8 +24,8 @@ type ConversationNoteRequest struct {
 }
 
 func (r *ConversationNoteRequest) Validate() error {
-	if len(r.Content) < 1 {
-		return errors.New("content is required")
+	if len(r.Content) < 1 || len(r.Content) > 1000 {
+		return errors.New("content must have number of characters between 1 and 1000")
 	}
 	if len(r.AppId) < 1 {
 		return errors.New("app id is required")
