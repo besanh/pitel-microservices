ad92ba7b2f136b92a67366b1738bea4645f19fc2
truongnguyentel4vn <truongnguyen@tel4vn.com>
2024-10-02 11:41:34 +0700

    update - simplify convert time millisecs format

--- common/util/converter.go
+++ common/util/converter.go
@@ -17,15 +17,10 @@ func ConvertToBytes(message any) ([]byte, error) {
 }
 
 func ConvertMillisToTimeString(millis int) string {
-	// Convert the milliseconds to a time.Duration (time.Duration expects nanoseconds)
 	duration := time.Duration(millis) * time.Millisecond
-
-	// Extract hours, minutes, seconds, and milliseconds
-	hours := int(duration.Hours())
-	minutes := int(duration.Minutes()) % 60
-	seconds := int(duration.Seconds()) % 60
-	milliseconds := millis % 1000 // Get the remainder to capture only milliseconds
+	t := time.Time{}.Add(duration)
 
 	// Format the string as hh:mm:ss.milliseconds
-	return fmt.Sprintf("%02d:%02d:%02d.%03d", hours, minutes, seconds, milliseconds)
+	// Use .Format to format up to seconds, and manually append milliseconds
+	return fmt.Sprintf("%s.%03d", t.Format("15:04:05"), millis%1000)
 }
